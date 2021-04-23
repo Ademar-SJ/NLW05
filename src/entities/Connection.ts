3fbc4508da124fdfc401ff24ac684406101c51b1
Ademar-SJ <ademarjr58@gmail.com>
2021-04-22 21:27:28 -0300

    Adicionado entidade Connection

---
+++ src/entities/Connection.ts
@@ -0,0 +1,36 @@
+import { Entity,Column,PrimaryColumn,CreateDateColumn,UpdateDateColumn,
+         ManyToOne,JoinColumn } from "typeorm";
+import { v4 as uuid } from 'uuid';
+import { User } from '../entities/User';
+
+@Entity("conections")
+class Connection {
+
+  @PrimaryColumn()
+  id : string;
+
+  @Column()
+  admin_id : string;
+  
+  @Column()
+  user_id : string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+  
+  @Column()
+  socket_id : string;
+  
+  @CreateDateColumn()
+  created_at : Date;
+
+  @UpdateDateColumn()
+  updated_at : Date;
+
+  constructor() {
+    if (!this.id) this.id = uuid();
+  }
+}
+
+export { Connection }
